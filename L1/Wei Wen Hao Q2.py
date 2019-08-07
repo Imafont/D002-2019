98668bb2f6704a584ceaa3b695d977628c0e0041
Imafont <53845619+Imafont@users.noreply.github.com>
2019-08-07 12:36:04 +0800

    I added def to all of them because I wanted to get use this function

---
+++ L1/Wei Wen Hao Q2.py
@@ -0,0 +1,19 @@
+from math import*
+def calculations(x):
+    print("The answer to questions 2 is:")
+    print(8*3.57)
+    print(5 + 30*20)
+    print((5 + 30)*20)
+    print(1+ (2 + 20*3)/(4*2))
+    print(1+2*10)
+    print(int(29/4)+1)
+
+def square_area(x):
+    r = sqrt(100/pi)
+    square = (2*r+1)**2
+    print "The radius of the circle and the area of the square is:"
+    print(r, square)
+
+calculations(1)
+square_area(1)
+
---
+++ L1/Wei Wen Hao Q3.py
@@ -0,0 +1,8 @@
+from math import*
+def circle_area(x):
+    r = float(input("What is the radius of the circle?"))
+    answer =(r**2)*pi
+    print("The area of the cirlce is %.2f" % answer)
+
+
+circle_area(1)
