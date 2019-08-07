d8c69eb1c0d48ecc5bae5dfc391f485ca657e4b8
Imafont <53845619+Imafont@users.noreply.github.com>
2019-08-07 11:57:20 +0800

    Add files via upload

---
+++ L1/Wei Wen Hao Q2.py
@@ -0,0 +1,11 @@
+def calculations(x):
+    print("The answer to questions 2 is:")
+    print(8*3.57)
+    print(5 + 30*20)
+    print((5 + 30)*20)
+    print(1+ (2 + 20*3)/(4*2))
+    print(1+2*10)
+    print(int(29/4)+1)
+
+calculations(1)
+
