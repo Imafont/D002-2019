d7a4238d208783745d09033bc4258878a76efd39
Imafont <53845619+Imafont@users.noreply.github.com>
2019-08-14 11:26:31 +0800

    Update Wei Wen Hao L3Q3.py

--- L3/Wei Wen Hao L3Q3.py
+++ L3/Wei Wen Hao L3Q3.py
@@ -1,12 +1,9 @@
 def words(x):
     l = []
-    check = 0
-    while check < 10:
-        check += 1
-        w = input("Enter a word:\n")
-        if (w[0].lower() == "a" or w[0].lower() == "e" or w[0].lower() == "i" or w[0].lower() =="o" or w[0].lower() =="u"):
+    for i in range(0, 10):
+        w = input("%i Enter a word:" %i)
+        first = w[0]
+        if first in "AEIOUaeiou":
             l.append(w)
-        else:
-            print('no')
     print(l)
 words(1)
