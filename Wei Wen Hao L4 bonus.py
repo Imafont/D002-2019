1442afa3c28dd8747ca0c1a4eb333d209254f245
Imafont <53845619+Imafont@users.noreply.github.com>
2019-08-14 13:52:00 +0800

    Delete Wei Wen Hao L4 bonus.py

--- Wei Wen Hao L4 bonus.py
+++
@@ -1,9 +0,0 @@
-print("rectangle(Top left coordinate, bottom right coordinate, second top left coordinate, second bottom right coordinate")
-def rectangle(a1, c1, a2, c2):
-    if ((a1[0] > c2[0] or a2[0] > c1[0]) or (a1[1] < c2[1] or a2[1] < c1[1])):
-        print("The rectangles will not overlap.")
-        return
-    print("The rectangles will overlap.")
-    return
-
-
