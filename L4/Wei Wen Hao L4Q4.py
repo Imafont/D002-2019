4029f706b10993a8795d01ead30e0f3c2d8ebe96
Imafont <53845619+Imafont@users.noreply.github.com>
2019-08-15 10:04:57 +0800

    Update Wei Wen Hao L4Q4.py

--- L4/Wei Wen Hao L4Q4.py
+++ L4/Wei Wen Hao L4Q4.py
@@ -1,6 +1,6 @@
 def checker(sentence, letter):
     result = []
-    for i in range(0, int(len(sentence)-1)):
+    for i in range(0, len(sentence)):
         if letter.lower() in sentence[i].lower():
             result.append(i)
     return result
